094545a4fcaf59fba50eba58daaf94ae102a54c3
MomoCow <momocow.me@gmail.com>
2018-09-20 22:58:40 +0800

    ci(release): refine CI release steps
    1. Create release tags. | 2. Push tags. | 3. Test | 4. Build and release. | 5. Push deploy. | 6.
    Merge to master and push.

---
+++ travis/git-setup.sh
@@ -0,0 +1,3 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add origin-for-travis https://${GH_TOKEN}@github.com/momocow/tukiyomi.git > /dev/null 2>&1
--- travis/release.sh
+++ travis/release.sh
@@ -1,11 +1,7 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git remote add origin-for-travis https://${GH_TOKEN}@github.com/momocow/tukiyomi.git > /dev/null 2>&1
-
 git checkout deploy
-git push --quiet --set-upstream origin-for-travis deploy
+git push origin-for-travis deploy
 
 git pull origin-for-travis master
 git checkout master
 git merge deploy
-git push --quiet --set-upstream origin-for-travis master
+git push origin-for-travis master
