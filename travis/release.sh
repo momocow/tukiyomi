5debe7083b373d4b1414025a48a32fac20f4f013
MomoCow <momocow.me@gmail.com>
2018-09-20 21:33:55 +0800

    ci(release): bump the version before build starts

--- travis/release.sh
+++ travis/release.sh
@@ -3,7 +3,6 @@ git config --global user.name "Travis CI"
 git remote add origin-for-travis https://${GH_TOKEN}@github.com/momocow/tukiyomi.git > /dev/null 2>&1
 
 git checkout deploy
-npm run vers-bump
 git push --quiet --set-upstream origin-for-travis deploy
 
 git pull origin-for-travis master
